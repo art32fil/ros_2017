2cd22aff3fae0ba0195f6565704ba5b6f73a549e
Konstantin <pro100kot14@gmail.com>
2017-10-06 14:17:49 +0300

    Add 2nd lab

---
+++ 2304/Chayka/lab_2/student/src/math.cpp
@@ -0,0 +1,43 @@
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+#include "std_srvs/Empty.h"
+#include <sstream>
+#include <fstream>
+
+bool passed = true;
+
+bool pass(std_srvs::Empty::Request  &req,
+         std_srvs::Empty::Response &res){
+  passed = true;
+  return true;
+}
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "math");
+  ros::NodeHandle n;
+  ros::Publisher chatter_pub = n.advertise<std_msgs::String>("debts", 1);
+  ros::Publisher last_word = n.advertise<std_msgs::String>("expel", 1);
+  ros::ServiceServer service = n.advertiseService("math/pass_the_exam", pass);
+  sleep(1);
+  ros::Rate loop_rate(10);
+  std::ifstream ifs("/dev/urandom", std::ifstream::in);
+  std::srand(ifs.get());
+  ifs.close();
+  std_msgs::String msg;
+  msg.data = "math";
+  while (ros::ok() && passed)
+  {
+    chatter_pub.publish(msg);
+    passed = false;
+    int border = 10 * (10 + std::rand()%30);
+    for(int i=0;i<border;i++){
+      ros::spinOnce();
+      if(!ros::ok())
+        break;
+      loop_rate.sleep();
+    }
+  }
+  last_word.publish(msg);
+  return 0;
+}
