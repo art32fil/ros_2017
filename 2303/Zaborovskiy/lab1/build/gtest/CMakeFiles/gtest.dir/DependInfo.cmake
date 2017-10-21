7a099d6c73de2bccd2fb8f3499eeedd69406acbd
KVinS <kvins-java@yandex.ru>
2017-10-21 06:03:51 +0300

    Вторая лабораторная

--- 2303/Zaborovskiy/lab1/build/gtest/CMakeFiles/gtest.dir/DependInfo.cmake
+++ 2303/Zaborovskiy/lab1/build/gtest/CMakeFiles/gtest.dir/DependInfo.cmake
@@ -17,7 +17,6 @@ set(CMAKE_TARGET_DEFINITIONS_CXX
 # The include file search paths:
 set(CMAKE_CXX_TARGET_INCLUDE_PATH
   "/home/kvins/ROS/lab1/include"
-  "/opt/ros/kinetic/include"
   "/usr/src/gtest/include"
   "/usr/src/gtest"
   )
