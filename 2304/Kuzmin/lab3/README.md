755bfde3a209a098aabfffaa0f8f5880269a45d8
Maxim Kuzmin <max_kuzmin@outlook.com>
2017-10-29 01:54:57 +0300

    Added some stuff to lab3

---
+++ 2304/Kuzmin/lab3/README.md
@@ -0,0 +1,7 @@
+How to use: "roslaunch battlebots fun.launch".
+
+Use arrows to move green bot.
+You need to hit red bot with green "finger".
+Score will appear in console.
+
+Tested on ROS Indigo and Gazebo 7.
--- 2304/Kuzmin/lab3/src/battlebots/src/bot2_tflistener_node.cpp
+++ 2304/Kuzmin/lab3/src/battlebots/src/bot2_tflistener_node.cpp
@@ -7,7 +7,6 @@
 
 using namespace ros;
 
-
 int main(int argc, char** argv)
 {
     init(argc, argv, "bot2_ai_node");
@@ -22,27 +21,54 @@ int main(int argc, char** argv)
 
     ROS_INFO("Bot2 tf listener works!");
 
-    while (ok()) {
+    int your_score=0, enemy_score=0;
 
-        geometry_msgs::TransformStamped ts2;
-            try{
-              ts2 = tfBuffer.lookupTransform("bot2", "bot1", ros::Time(0));
-            }
-            catch (tf2::TransformException &ex) {
-              ROS_WARN("%s",ex.what());
-              ros::Duration(1.0).sleep();
-              continue;
-            }
+    while (ok()) {
+        geometry_msgs::TransformStamped ts;
+        try {
+            ts = tfBuffer.lookupTransform("bot2", "bot1", ros::Time(0));
 
             geometry_msgs::Twist vel_msg;
-
             //=0-4*atan2(deltaX, deltaY)
-            vel_msg.angular.z = -4.0 * atan2(ts2.transform.translation.y, ts2.transform.translation.x);
-
+            vel_msg.angular.z = -2.0 * atan2(ts.transform.translation.y, ts.transform.translation.x);
             //=1+0.2*sqrt(deltaX^2+deltay^2)
-            vel_msg.linear.x = 1+0.2 * sqrt(pow(ts2.transform.translation.x, 2) + pow(ts2.transform.translation.y, 2));
-
+            vel_msg.linear.x = 1 + 0.2 * sqrt(pow(ts.transform.translation.x, 2) + pow(ts.transform.translation.y, 2));
             publisher1.publish(vel_msg);
+        }
+        catch (tf2::TransformException& ex) {
+            ROS_WARN("%s", ex.what());
+            ros::Duration(1.0).sleep();
+        }
+
+        //=====================
+
+        try {
+            ts = tfBuffer.lookupTransform("bot2::link_box3", "bot1::link_base", ros::Time(0));
+
+            if (sqrt(pow(ts.transform.translation.x, 2) + pow(ts.transform.translation.y, 2)) < 1) {
+                enemy_score++;
+                ROS_INFO("Score. You: %d - Enemy: %d", your_score, enemy_score);
+            }
+        }
+        catch (tf2::TransformException& ex) {
+            ROS_WARN("%s", ex.what());
+            ros::Duration(1.0).sleep();
+        }
+
+        //=====================
+
+        try {
+            ts = tfBuffer.lookupTransform("bot1::link_box3", "bot2::link_base", ros::Time(0));
+
+            if (sqrt(pow(ts.transform.translation.x, 2) + pow(ts.transform.translation.y, 2)) < 1) {
+                your_score++;
+                ROS_INFO("Score. You: %d - Enemy: %d", your_score, enemy_score);
+            }
+        }
+        catch (tf2::TransformException& ex) {
+            ROS_WARN("%s", ex.what());
+            ros::Duration(1.0).sleep();
+        }
 
         loop_rate.sleep();
     }
